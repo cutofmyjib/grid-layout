40b81cf2544e2f45ff9cbfd13d3acc8a81809d31
cutofmyjib <thedianalozano@gmail.com>
2015-11-19 21:12:24 -0800

    add comment on js function

--- js/filter-menu.js
+++ js/filter-menu.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   selectionFilterMenu();
 });
 
+//add text to span element in h4 tag in menu div
 function selectionFilterMenu() {
   $('.menu-ul li').on('click', function(e){
     $('.filter-selection').text(e.target.textContent);
